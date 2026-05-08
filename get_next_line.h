ba36c80b495db7f1eae0479271ac940c4e40e7a3
Filipe Alves Esteves De Almeida Cabrito <falves-e@c1r4s1.42lisboa.com>
2026-05-08 12:34:46 +0100

    first_commit

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falves-e <falves-e@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 12:29:11 by falves-e          #+#    #+#             */
+/*   Updated: 2026/05/08 12:31:49 by falves-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 10
+#endif 
+
+char	*get_next_line(int fd);
+
+#endif
